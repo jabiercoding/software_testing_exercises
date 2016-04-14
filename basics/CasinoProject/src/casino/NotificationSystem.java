dd421d07824f7743584ba402878ca935bad1f3aa
jabier.martinez <jabimail@gmail.com>
2016-04-14 14:08:29 +0200

    missing class

---
+++ basics/CasinoProject/src/casino/NotificationSystem.java
@@ -0,0 +1,17 @@
+package casino;
+
+/**
+ * Notification system
+ */
+public class NotificationSystem {
+
+	/**
+	 * An alert will be sent to the director of the Casino
+	 * 
+	 * @param message
+	 */
+	public void sendAlertToTheDirector(String message) {
+		System.out.println("Sending alert to the director: " + message);
+	}
+
+}
